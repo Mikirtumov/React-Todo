958e691b907d33628a68e9342c2538e587d3be19
Mikirtumov <you@example.com>
2020-05-28 01:41:56 -0700

    TodoList.js and TodoCreateForm.js files added

---
+++ src/TodoCreateForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function TodoCreateForm() {
+  return (
+    <div>
+        List
+    </div>
+  );
+}
+
+export default TodoCreateForm;
---
+++ src/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function TodoList() {
+  return (
+    <div>
+        List
+    </div>
+  );
+}
+
+export default TodoList;
