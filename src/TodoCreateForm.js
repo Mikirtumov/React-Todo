a5b360d43ed5ba651db8eb8791cf0dd083b5b05b
Mikirtumov <you@example.com>
2020-06-02 04:54:46 -0700

    on button addtask trim added

--- src/TodoCreateForm.js
+++ src/TodoCreateForm.js
@@ -13,7 +13,7 @@ function TodoCreateForm(props) {
     return (
         <div>
             <input type="text" value={task} onChange={e => setTask(e.target.value)} />
-            <button onClick={addTask}>Add Task</button>
+            <button onClick={addTask} disabled={task.trim() === ''}>Add Task</button>
         </div>
     );
 }
